b6fcde8f736f6b7b8f53931651820454250e103d
Christopher Hemmens <129729158+hemmens@users.noreply.github.com>
2024-06-09 09:58:10 +0200

    Add files via upload

---
+++ knockoff_lib2.py
@@ -0,0 +1,229 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Oct 24 15:22:44 2023
+
+@author: Christopher Hemmens
+"""
+
+import numpy as np
+from scipy.stats import kstest, beta, gamma, norm, truncnorm, t, \
+                        uniform, loglaplace, multivariate_normal#, skew, kurtosis
+from scipy.optimize import minimize, NonlinearConstraint
+
+candidates = {'BetaUnivariate': {'str': 'beta', 'fn': beta},
+              'GammaUnivariate': {'str': 'gamma', 'fn': gamma},
+              'GaussianUnivariate': {'str': 'norm', 'fn': norm},
+              'TruncatedGaussian': {'str': 'truncnorm', 'fn': truncnorm},
+              'StudentTUnivariate': {'str': 't', 'fn': t},
+              'UniformUnivariate': {'str': 'uniform', 'fn': uniform},
+              'LogLaplace': {'str': 'loglaplace', 'fn': loglaplace}}
+
+def coskew(m, how='left') :
+    X = m.copy()
+    n, p = X.shape
+    
+    X -= X.mean(axis=0)
+    X /= X.std(axis=0)
+    Y = X.copy()
+    
+    if how == 'right' :
+        output = np.dot(X.T,Y**2)
+    else :
+        output = np.dot((X**2).T,Y)
+    
+    return output/n
+    
+def cokurt(m, how='center') :
+    X = m.copy()
+    n, p = X.shape
+    
+    X -= X.mean(axis=0)
+    X /= X.std(axis=0)
+    Y = X.copy()
+    
+    if how == 'right' :
+        output = np.dot(X.T,Y**3)
+    elif how == 'left' :
+        output = np.dot((X**3).T,Y)
+    else :
+        output = np.dot((X**2).T,Y**2)
+    
+    return output/n
+
+def get_con_value(m, feature, to_zero='triangle') :
+    x = m.copy()
+    x -= feature
+    if to_zero[:4] == 'diag' :
+        np.fill_diagonal(x, 0)
+    else :
+        x = np.tril(x, k=-1)
+        
+    x = x**2
+    
+    return np.sum(x)
+
+def is_mean(x, feature) :
+    p = feature.shape[0]
+    n = x.shape[0] // p
+    xc = x.copy()
+    xc = xc.reshape(n,p)
+    xc = xc.mean(axis=0)
+    xc -= feature
+    xc = xc**2
+        
+    return sum(xc)
+
+def is_var(x, feature) :
+    p = feature.shape[0]
+    n = x.shape[0] // p
+    xc = x.copy()
+    xc = xc.reshape(n,p)
+    xc = xc.var(axis=0)
+    xc -= feature
+    xc = xc**2
+        
+    return sum(xc)
+
+def is_distributed(x, dists) :
+    p = len(dists)
+    n = x.shape[0] // p
+    xc = x.copy()
+    xc = xc.reshape(n,p)
+    
+    xc = np.array([1 - kstest(xc[:,kk],
+                    candidates[dists[kk]['type'].split('.')[-1]]['str'],
+                    (dists[kk]['a'], dists[kk]['b'],
+                         dists[kk]['loc'], dists[kk]['scale'])).pvalue
+                   if dists[kk]['type'].split('.')[-1] == 'BetaUnivariate'
+                   else 
+                   1 - kstest(xc[:,kk],
+                            candidates[dists[kk]['type'].split('.')[-1]]['str'],
+                            tuple(dists[kk].values())[:-1]).pvalue
+                   for kk in range(p)])
+    xc = xc**2
+        
+    return sum(xc)/p
+
+def corr_match(x, feature) :
+    n, p = feature.shape
+    xc = x.copy()
+    xc = xc.reshape(n, p)
+    corr = np.corrcoef(feature.T, xc.T)
+    
+    real = corr[:p,:p].copy()
+    knockoff = corr[p:,p:].copy()
+    corr = corr[:p,p:].copy()
+    
+    corr = get_con_value(corr, real, 'diag')
+    knockoff = get_con_value(knockoff, real, 'triangle')
+    
+    return (corr + knockoff)
+
+def cosk_match(x,feature) :
+    n, p = feature.shape
+    xc = x.copy()
+    xc = xc.reshape(n,p)
+    cosk = coskew(np.hstack((feature, xc)))
+    
+    real = cosk[:p,:p].copy()
+    knockoff = cosk[p:,p:].copy()
+    lcosk = cosk[:p,p:].copy()
+    rcosk = cosk[p:,:p].copy()
+    
+    lcosk = get_con_value(lcosk, real, 'diag')
+    rcosk = get_con_value(rcosk, real, 'diag')
+    knockoff = get_con_value(knockoff, real, 'diag')
+                
+    return (lcosk + rcosk + knockoff)
+
+def coku_match(x,feature) :
+    n, p = feature.shape
+    xc = x.copy()
+    xc = xc.reshape(n,p)
+    # Center Kurtosis
+    coku_c = cokurt(np.hstack((feature, xc)), 'center')
+    
+    real_c = coku_c[:p,:p].copy()
+    knockoff_c = coku_c[p:,p:].copy()
+    ccoku = coku_c[:p,p:].copy()
+    
+    ccoku = get_con_value(ccoku, real_c, 'diag')
+    knockoff_c = get_con_value(knockoff_c, real_c, 'triangle')
+    
+    # Left and Right Kurtosis
+    coku = cokurt(np.hstack((feature, xc)), 'left')
+    
+    real = coku[:p,:p].copy()
+    knockoff = coku[p:,p:].copy()
+    lcoku = coku[:p,p:].copy()
+    rcoku = coku[p:,:p].copy()
+    
+    lcoku = get_con_value(lcoku, real, 'diag')
+    rcoku = get_con_value(rcoku, real, 'diag')
+    knockoff = get_con_value(knockoff, real, 'diag')
+                
+    return (ccoku + lcoku + rcoku + knockoff_c + knockoff)
+
+def get_cov_min_eigval(x, covs_inv) :
+    x_diag = np.diag(x)
+    covs_t = np.dot(x_diag, np.dot(covs_inv, x_diag))
+    return min(np.linalg.eigvals(2*x_diag - covs_t))
+
+def get_diag_weights(x, vs) :
+    return sum([y**2 for y in vs-x])
+
+def get_candes(feature, dists) :
+    x = feature.copy()
+    n, p = x.shape
+    for kk in range(p) :
+        if dists[kk]['type'].split('.')[-1] == 'BetaUnivariate' :
+            x[:,kk] = beta.cdf(feature[:,kk], dists[kk]['a'], dists[kk]['b'],
+                               dists[kk]['loc'], dists[kk]['scale'])
+        else :
+            x[:,kk] = candidates[dists[kk]['type'].split('.')[-1]]['fn'].cdf(feature[:,kk],
+                                                            *tuple(dists[kk].values())[:-1])
+        
+    x = norm.ppf(x)
+    x = x.T
+    
+    covs = np.cov(x)
+    vs = np.diag(covs)
+    covs_inv = np.linalg.inv(covs)
+
+    rng = np.random.default_rng(24)
+
+    while True :
+        thresh = rng.uniform(0,1e-4)
+        min_eigval = NonlinearConstraint(lambda x: get_cov_min_eigval(x, covs_inv),
+                                         thresh, np.inf)
+        
+        res = minimize(lambda x: get_diag_weights(x, vs),
+                       np.zeros(x.shape[0]),
+                       constraints=min_eigval)
+        
+        if get_cov_min_eigval(res.x, covs_inv) >= 0 :
+            break
+        
+    x_diag = np.diag(res.x)
+
+    samples = []
+    for kk in range(n) :
+        samples += [multivariate_normal.rvs(x[:,kk] - np.dot(x[:,kk], np.dot(covs_inv, x_diag)),
+                                            2*x_diag - np.dot(x_diag, np.dot(covs_inv, x_diag)))]
+            
+    samples = np.array(samples)
+    samples = norm.cdf(samples)
+    for kk in range(p) :
+        if dists[kk]['type'].split('.')[-1] == 'BetaUnivariate' :
+            samples[:,kk] = beta.ppf(samples[:,kk], dists[kk]['a'], dists[kk]['b'],
+                                     dists[kk]['loc'], dists[kk]['scale'])
+        else :
+            samples[:,kk] = candidates[dists[kk]['type'].split('.')[-1]]['fn'].ppf(samples[:,kk],
+                                                                *tuple(dists[kk].values())[:-1])
+    
+    return samples
+    
+    
+    
+    
---
+++ t_hemmens.py
@@ -0,0 +1,163 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Mar 15 15:49:31 2024
+
+@author: Christopher Hemmens
+"""
+
+import math
+import numpy as np
+import time
+import datetime as dt
+import pickle as pkl
+from scipy.stats import t
+
+import t_core
+import knockoff_lib2 as ko
+
+#simulation parameters
+df_t = 5 # degree of freedom of t-distribution
+p = 30 # dimension of the random vector X
+numsamples = 200 # number of samples to generate knockoffs 
+rhos = [0.3] * (p-1) # the correlations
+
+#algorithm parameters
+halfnumtry = 1 # m/half number of candidates
+stepsize = 1.5 # step size in the unit of 1/\sqrt((\Sigma)^{-1}_{jj})
+
+#generate the proposal grid
+quantile_x = np.zeros([p, 2*halfnumtry + 1])
+sds = [0]*p
+sds[0] = math.sqrt(1 - rhos[0]**2)
+for i in range(1,p - 1):
+    sds[i] = math.sqrt((1 - rhos[i - 1]**2)*(1 - rhos[i]**2) /
+                       (1 - rhos[i - 1]**2*rhos[i]**2))
+sds[p - 1] = math.sqrt(1 - rhos[p - 2]**2)
+for i in range(p):
+    quantile_x[i] = [x*sds[i]*stepsize for x in list(
+        range(-halfnumtry, halfnumtry + 1))]
+    
+bigmatrix = np.zeros([numsamples, 2*p]) # store simulation data
+
+#generate each observation and knockoff
+start = time.time()
+for i in range(numsamples):
+    #sample one instance from the Markov Chain
+    bigmatrix[i, 0] = t.rvs(df=df_t)*math.sqrt((df_t - 2)/df_t)
+    for j in range(1, p):
+        bigmatrix[i, j] = math.sqrt(1 - rhos[j - 1]**2)*t.rvs(df=df_t)* \
+        math.sqrt((df_t - 2)/df_t) + rhos[j - 1]*bigmatrix[i,j - 1]
+    
+    #sample the knockoff for the observation
+    bigmatrix[i, p:(2*p)] = t_core.SCEP_MH_MC(bigmatrix[i, 0:p], 0.999, 
+                                       quantile_x, rhos, df_t)
+end = time.time()
+print(round(end - start,1))
+
+test = bigmatrix[:, :p].copy()
+ko_mc = bigmatrix[:, p:].copy()
+
+print('Fitting copula...')
+from copulas.multivariate import GaussianMultivariate
+from copulas.univariate import ParametricType, Univariate
+from scipy.optimize import minimize, NonlinearConstraint, LinearConstraint
+
+import warnings
+warnings.filterwarnings("ignore")
+
+univariate = Univariate(parametric=ParametricType.PARAMETRIC)
+copula = GaussianMultivariate(distribution=univariate)
+copula.fit(test)
+params = copula.to_dict()
+
+test_mean = test.mean(axis=0)
+test_var = test.var(axis=0)
+#test_pv = np.array([kstest(test[:,kk],'t',tuple(params['univariates'][kk].values())[:-1]).pvalue for kk in range(test.shape[1])])
+
+n, p = test.shape
+
+A = np.zeros((p, p*n))
+for kk in range(p) :
+    A[kk, n*kk:n*(kk+1)] = 1/n
+    
+xmean = LinearConstraint(A, test_mean, test_mean)
+xvar = NonlinearConstraint(lambda x: ko.is_var(x, test_var), 0, 0)
+xdist = NonlinearConstraint(lambda x: \
+                    ko.is_distributed(x, params['univariates']), 0, 0.25)
+    
+corr_con = NonlinearConstraint(lambda x: ko.corr_match(x, test), 0, 0)
+cosk_con = NonlinearConstraint(lambda x: ko.cosk_match(x, test), 0, 0)
+coku_con = NonlinearConstraint(lambda x: ko.coku_match(x, test), 0, 0)
+                
+constraints = (xmean,
+               xvar,
+               xdist,
+               corr_con,
+               cosk_con,
+               coku_con
+               )
+
+# Minimize sum of squared correlation between features and knockoffs
+def squared_corr(x) :
+    xc = x.copy()
+    xc = xc.reshape(n,p)
+    corr = np.corrcoef(xc.T, test.T)
+    corr = corr[:p,p:].copy()
+    corr = np.diag(corr)
+    corr = corr**2
+        
+    return sum(corr)
+
+print('Getting Candes knockoff...')
+# Generate Initial Guess
+x0 = ko.get_candes(test, params['univariates'])
+x0 = x0.reshape(-1,)
+
+output = {}
+output['x0'] = x0.copy()
+output['params'] = params
+
+# Optimization
+max_iter = [1,2,3,5,10,20]
+
+output = {}
+
+output['features'] = test.copy()
+output['ko_mc'] = ko_mc.copy()
+output['time'] = end - start
+
+for mi in max_iter :
+    output[mi] = {}
+    output[mi]['x0'] = x0.copy()
+    output[mi]['params'] = params
+    
+    print(f'Running optimisation... {mi}')
+    # Optimization
+    start = dt.datetime.now()
+    print('Start:')
+    print(start)
+    res = minimize(squared_corr, x0,
+                   constraints=constraints,
+                   tol=1e-3,
+                   options={'maxiter': mi,
+                            'disp': True})
+    
+    end = dt.datetime.now()
+    print('End:')
+    print(end)
+    
+    output[mi]['start'] = start
+    output[mi]['end'] = end
+    output[mi]['time'] = (end-start).seconds
+    
+    ko_cnd = x0.reshape(n,p)
+    
+    x1 = res.x
+    ko_hms = x1.reshape(n,p)
+    
+    output[mi]['x1'] = x1.copy()
+    output[mi]['Candes'] = ko_cnd.copy()
+    output[mi]['Hemmens'] = ko_hms.copy()
+    
+    with open('KOMC_03.pkl', 'wb') as fp :
+        pkl.dump(output, fp)
